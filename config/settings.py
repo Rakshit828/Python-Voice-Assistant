b2f4c00d7bc1ed6c61718573ececc5510876a9b2
Rakshit828 <horizonsjf14@gmail.com>
2025-08-03 19:36:29 +0545

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -4,10 +4,14 @@ import os
 
 load_dotenv()  # Loads from .env in root directory
 
-WEATHER_API_KEY = os.getenv("WEATHER_API_SHRIJAN")
+WEATHER_API_KEY = os.getenv("WEATHER_API")
 GROQ_API_KEY = os.getenv('GROQ_API')
-DEEPSEEK_V3_0324_API_KEY = os.getenv('DEEPSEEK_V3_0324')
-DEEPSEEK_R1_API_KEY = os.getenv("DEEEPSEEK R1")
 
-NEWS_API_KEY = os.getenv("NEWS_API")
+
+# These are currently not in use
+# DEEPSEEK_V3_0324_API_KEY = os.getenv('DEEPSEEK_V3_0324')
+# DEEPSEEK_R1_API_KEY = os.getenv("DEEEPSEEK R1")
+
+# NEWS_API_KEY = os.getenv("NEWS_API")
+
 
